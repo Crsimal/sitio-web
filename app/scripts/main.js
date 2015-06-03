142dff4f90809e578f11d15a608f543400e60151
Crsimal <cros@cros-IMEDIA-6550-AIO.(none)>
2015-06-03 18:42:28 +0200

    Secciones creadas, multimedia y eventos

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,12 @@
 /* jshint devel:true */
 console.log('\'Allo \'Allo!');
+
+$(document).ready(function(){
+    //FANCYBOX
+    //https://github.com/fancyapps/fancyBox
+    $(".fancybox").fancybox({
+        openEffect: "none",
+        closeEffect: "none"
+    });
+});
+   
